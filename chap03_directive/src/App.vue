78f64a06e1214e05a71627d67d58ddb0d3d8e93b
wooilgu <madanggu@gmail.com>
2026-05-19 10:50:37 +0900

    A03Attribute.vue - 속성을 변경하기 위한 수업 ( 단방향 바인드 ) - 양방향 바인드 - v-model은 사용자 맞춤(커스터마이징)이 가능하다. --> chatgpt로 물어보자.

--- chap03_directive/src/App.vue
+++ chap03_directive/src/App.vue
@@ -5,16 +5,21 @@ import IconOne from './components/icons/IconCommunity.vue';
 
 import A01Ref from './components/A01Ref.vue'
 import A02Reactive from './components/A02Reactive.vue'
+import A03Attribute from './components/A03Attribute.vue'
+import A04MakeDOM from './components/A04MakeDOM.vue'
+
 </script>
 
 <template>
   <div class="m-3">
     <h1>
       <IconOne />
-      Chap03 Directive
+      A04MakeDOM
     </h1>
 
     <div>
+      <A04MakeDOM></A04MakeDOM>
+      <A03Attribute></A03Attribute>
       <A02Reactive></A02Reactive>
       <!-- <A01Ref></A01Ref> -->
     </div>
