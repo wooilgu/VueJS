85b0c5860b33f6d1c57df919d7f0746277c738b2
wooilgu <madanggu@gmail.com>
2026-05-22 11:47:15 +0900

    pinia를 사용하기 시작함

---
+++ chap08_pinia/src/components/TodoTable.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TodoItem from './TodoItem.vue';
+</script>
+
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th :style="{width:'10%'}">ID</th>
+          <th>Todo</th>
+          <th :style="{width:'10%'}">Complete</th>
+          <th :style="{width:'10%'}">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template>
+          <TodoItem />
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
